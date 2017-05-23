825f50b5b8bd97ef58201982fa433333e7a33426
Maximilian Carr <maximilianacarr@gmail.com>
2017-05-23 14:20:37 -0400

    created user class and average likes

--- insta.py
+++ insta.py
@@ -3,15 +3,36 @@
 import json
 import requests
 import csv
+import sys
+
+class User:
+    def __init__(self, username):
+        data = getuserJSON(username)
+        if(data):
+            self.data = getuserJSON(username)
+        else:
+            raise LookupError
+    def getaveragelikes(self, start=-sys.maxsize, end=sys.maxsize):
+        """returns the average number likes between start and end"""
+        total = n = 0
+        for photo in self.data['items']:
+            if int(photo['created_time']) > start and int(photo['created_time']) < end:
+                total += photo['likes']['count']
+                n += 1
+        return total/n
 
 def getuserJSON(username):
+    """returns user object from username"""
+    page = requests.get('https://www.instagram.com/'+username+'/media/')
     try:
-        page = requests.get('https://www.instagram.com/'+username+'/media/')
-    except JSONDecodeError:
+        data = json.loads(page.text)
+        return data
+    except json.decoder.JSONDecodeError:
         print('user not found')
-    return json.loads(page.text)
+        return False
 
 def getuserCSV(username):
+    """outputs a csv file with likes and timestamps from username"""
     data = getuserJSON(username)
 
     with open(username+'.csv', 'w') as outfile:
