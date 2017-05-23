8d9dccf3758b30556f3e8ddd2cd95871c413530a
Maximilian Carr <maximilianacarr@gmail.com>
2017-05-22 22:59:30 -0400

    added csv functionality

--- insta.py
+++ insta.py
@@ -2,7 +2,24 @@
 
 import json
 import requests
+import csv
 
 def getuserJSON(username):
-    page = requests.get('https://www.instagram.com/'+username+'/media/')
-    print(page.text)
+    try:
+        page = requests.get('https://www.instagram.com/'+username+'/media/')
+    except JSONDecodeError:
+        print('user not found')
+    return json.loads(page.text)
+
+def getuserCSV(username):
+    data = getuserJSON(username)
+
+    with open(username+'.csv', 'w') as outfile:
+        fieldnames =['time', 'likes', 'link']
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for photo in data['items']:
+            time = photo['created_time']
+            likes = photo['likes']['count']
+            link = photo['images']['standard_resolution']['url']
+            writer.writerow({'time': time, 'likes': likes, 'link': link})
